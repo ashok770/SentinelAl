31de803a3c7148f04481154c4ef5465a7b690ee1
ashok770 <ashoktamata6002@gmail.com>
2026-07-27 12:27:07 +0530

    implement investigation details API

--- backend/src/repositories/investigation.repository.js
+++ backend/src/repositories/investigation.repository.js
@@ -3,7 +3,15 @@ import Investigation from "../models/Investigation.js";
 
 export const createInvestigation = async (data) => Investigation.create(data);
 
-export const getInvestigationById = async (id) => Investigation.findById(id);
+export const getInvestigationById = async (id) => {
+  console.log("Requested Investigation ID:", id);
+
+  const investigation = await Investigation.findById(id);
+
+  console.log("Found Investigation:", investigation);
+
+  return investigation;
+};
 
 export const getEvidenceByInvestigationId = async (investigationId) =>
   Evidence.find({ investigationId }).sort({ timestamp: 1 });
