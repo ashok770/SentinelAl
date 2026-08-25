dd6796d634c1d12cd616d0f5ff151a7b471fc105
Student <student@gmail.com>
2026-08-25 21:15:15 +0530

    completed the ontegration of the fronted and backend investigation dasbhoard

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
